80676f1ae780446615522f32bbdc64dad5cb2877
arnazalavi <arnazalavi@gmail.com>
2021-10-04 16:05:12 -0500

    latest Changes

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -9,7 +9,7 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/phone_app")
+
 
 
 @app.route("/")
